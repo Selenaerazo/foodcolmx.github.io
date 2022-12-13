5bbc771f0b1e288fc34c4288f6ce9847e1c75a4b
selenadtroya <68613425+selenadtroya@users.noreply.github.com>
2022-12-13 16:46:46 -0600

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,6 @@
 (function(){
     
-    const sliders = [...document.querySelectorAll('.testimony__body')];
+    const sliders = [...document.querySelectorAll('.presentation__body')];
     const buttonNext = document.querySelector('#next');
     const buttonBefore = document.querySelector('#before');
     let value;   
@@ -14,17 +14,17 @@
     });
 
     const changePosition = (add)=>{
-        const currentTestimony = document.querySelector('.testimony__body--show').dataset.id;
-        value = Number(currentTestimony);
+        const currentPresentation = document.querySelector('.presentation__body--show').dataset.id;
+        value = Number(currentPresentation);
         value+= add;
 
 
-        sliders[Number(currentTestimony)-1].classList.remove('testimony__body--show');
+        sliders[Number(currentPresentation)-1].classList.remove('presentation__body--show');
         if(value === sliders.length+1 || value === 0){
             value = value === 0 ? sliders.length  : 1;
         }
 
-        sliders[value-1].classList.add('testimony__body--show');
+        sliders[value-1].classList.add('presentation__body--show');
 
     }
 
